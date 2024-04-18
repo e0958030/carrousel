ec5827c68e9e3779a125e4118cfac22e1d33ba6d
Camilia El Moustarih <119824587+e0958030@users.noreply.github.com>
2024-04-18 10:58:57 -0400

    s10c2 - désastre javascript

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,19 @@
 (function(){
-    console.log("Vive JavaScript");
+    //console.log("Vive JavaScript");
+    let carrousel = document.querySelector('.carrousel')
+    //console.log("conteneur carrousel =" + carrousel.tagName);
+
+    let bouton = document.querySelector('.bouton__ouvrir')
+    //console.log("bouton =" + bouton.tagName);
+    
+    let carrousel__x = document.querySelector('.carrousel__x')
+
+    bouton.addEventListener('mousedown', function(){
+        carrousel.classList.add('carrousel--ouvrir')
+    })
+        
+    carrousel__x.addEventListener('mousedown', function(){
+        carrousel.classList.remove('carrousel--ouvrir')
+    })
+
 })()
