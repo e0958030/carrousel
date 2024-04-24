07fa6c2df8e2ad4680e46e4e1e070dd0668531c8
Camilia El Moustarih <119824587+e0958030@users.noreply.github.com>
2024-04-24 10:51:08 -0400

    s11c1 - Ajout d'une boucle for of pour les boutons radio - non fonctionnelle pour l'instant

--- js/carrousel.js
+++ js/carrousel.js
@@ -11,6 +11,7 @@
     let galerie = document.querySelector('.galerie')
     console.log('galerie' + galerie.tagName)
     let carrousel__figure = document.querySelector('figure')
+    let carrousel__img = document.createElement('img')
     
     //Création dynamique d'une image du carrousel 
     let galerie__img = galerie.querySelectorAll('img')
@@ -33,7 +34,7 @@
     function creer_image_galerie(index, elm){
         console.log(elm.src) //vérifier l'adresse url
         //créer les images du carrousel
-        let carrousel__img = document.createElement('img')
+        
         carrousel__img.src = elm.src
         carrousel__img.classList.add('carrousel__img')
         //ajouter un attribut à la balise html
@@ -47,16 +48,39 @@
      * @param {*} index le numéro de l'image
      * @param elm l'élément image de la galerie
      */
-
-    function creer_radio_carrousel(index){
-        let carrousel_radio = document.createElement('input')
-        //Ajouter une classe 
+    
+    function creer_radio_carrousel(index){    
+        let carrousel__radio = document.createElement('input');
+        // Ajouter une classe 
+        carrousel__radio.classList.add('carrousel__radio');
         // ajouter un index
-        //ajouter un type radio
-        //spécifier name
-        //ajouter dans carrousel__form
-    }
+        carrousel__radio.dataset.index = index;
+        // ajouter un type radio
+        carrousel__radio.setAttribute('type', 'radio');
+        // spécifier name 
+        carrousel__radio.setAttribute('name', 'radio__carrousel');
+        // ajouter dans carrousel__form 
+        let carrousel__form = document.querySelector('.carrousel__form');
+        carrousel__form.appendChild(carrousel__radio);
+        //Ajouter un écouteur qui permettera de modifier l'opacité de l'image index
+        //carrousel__figure.children[index].style.opacity = 1
 
+        /* Écouteur pour capter les clics des boutons radio */
+        carrousel__radio.addEventListener('mousedown', function(){
+        console.log("Clic sur bouton radio");
+        })
+
+        //boucle for of pour chaque bouton radio
+        let boutonRadio = carrousel__radio
+        for(unBoutonRadio of lesBoutonsRadio){
+            if (index === selectedIndex) {
+                figure.style.opacity = 1;
+            } else {
+                figure.style.opacity = 0;
+            }
+        }
+    }
+    
     /*
     //Collection d'images de la galerie    
     console.log("premiere image du carrousel = " + carrousel__img.src)
@@ -64,14 +88,14 @@
     console.log(carrousel__figure)
     */
 
-/* écouteur pour ouvrir la boîte modale */
+    /* écouteur pour ouvrir la boîte modale */
     bouton.addEventListener('mousedown', function(){
         carrousel.classList.add('carrousel--ouvrir') // ouvrir le carrousel
         console.log("J'ai bien cliqué sur le bouton!!!!!");
     })
-/* Écouteur pour fermer la boîte modale */
+    /* Écouteur pour fermer la boîte modale */
     carrousel__x.addEventListener('mousedown', function(){
         carrousel.classList.remove('carrousel--ouvrir') // fermer le carrousel
-    })
+    })  
 
 })()
