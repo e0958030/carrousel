cf38d897242c87f04c6d18cb3eeafe091a4cd91e
Camilia El Moustarih <119824587+e0958030@users.noreply.github.com>
2024-04-24 09:43:52 -0400

    s11c1 - Ajout de boutons radio et correction du scss

--- carrousel.php
+++ carrousel.php
@@ -8,38 +8,81 @@
      * Author URI: https://e0958030.github.io/4w4-2024/
      */
 
-    function enqueue_style_script(){
+    // function enqueue_style_script(){
+    //     $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+    //     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    //     wp_enqueue_style(   'em_plugin_carrousel_css',
+    //                     plugin_dir_url(__FILE__) . "style.css",
+    //                     array(),
+    //                     $version_css);
+
+    //     wp_enqueue_script(  'em_plugin_carrousel_js',
+    //                     plugin_dir_url(__FILE__) ."js/carrousel.js",
+    //                     array(),
+    //                     $version_js,
+    //                     true);
+    // }
+
+    // add_action('wp_enqueue_scripts', 'enqueue_style_script');
+
+
+    // function genere_html(){
+
+    //     $html = '
+    //     <button class="bouton_ouvrir">Ouvrir Carrousel</button>
+    //     <div class="carrousel">
+    //         <a href="" class="carrousel__x">X</a>
+    //         <figure class="carrousel__figure"></figure>
+    //         <form action="" class="carrousel__form"></form>
+    //     </div>
+    //     ';
+    //     return $html;
+    // }
+
+    // add_shortcode('carrousel', 'genere_html');
+    
+
+    
+    
+     function enqueue_style_script(){
         $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
         $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
+    
         wp_enqueue_style(   'em_plugin_carrousel_css',
-                        plugin_dir_url(__FILE__) . "style.css",
-                        array(),
-                        $version_css);
-
+                         plugin_dir_url(__FILE__) . "style.css",
+                         array(),
+                         $version_css);
+    
         wp_enqueue_script(  'em_plugin_carrousel_js',
                         plugin_dir_url(__FILE__) ."js/carrousel.js",
                         array(),
                         $version_js,
                         true);
-    }
-
-    add_action('wp_enqueue_scripts', 'enqueue_style_script');
-
-
+    
+     }
+    
+    //  Il faut que wp_head soit inclut juste avant la balise </head>
+    // Et que wp_footer soit ajouté aussi juste avant la base </body>
+     add_action('wp_enqueue_scripts','enqueue_style_script');
+    
+    
+    
     function genere_html(){
-
+    
+    
+    
         $html = '
-        <button class="bouton_ouvrir">Ouvrir Carrousel</button>
+        <button class="bouton__ouvrir">Ouvrir Carrousel</button>
         <div class="carrousel">
-            <a href="" class="carrousel__x">X</a>
+            <button class="carrousel__x">X</button>
             <figure class="carrousel__figure"></figure>
             <form action="" class="carrousel__form"></form>
         </div>
         ';
         return $html;
     }
-
-    add_shortcode('carrousel', 'genere_html');
     
-
+    // [carrousel] juste après la galerie dans votre article ou page
+    // Quand la fonction the_content() rencontera [carrousel], le carrousel sera initialisé
+    add_shortcode('carrousel', 'genere_html');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,19 +1,77 @@
+// (fonction(){})() est une fonction auto éxécutante
+
 (function(){
-    //console.log("Vive JavaScript");
+    console.log("Vive Javascript")
     let carrousel = document.querySelector('.carrousel')
-    //console.log("conteneur carrousel =" + carrousel.tagName);
-
+    console.log("conteneur carrousel = " + carrousel.tagName)
     let bouton = document.querySelector('.bouton__ouvrir')
-    //console.log("bouton =" + bouton.tagName);
-    
+    console.log("bouton = " + bouton.tagName)
     let carrousel__x = document.querySelector('.carrousel__x')
+    console.log('carrousel__x' + carrousel__x.tagName)
+    let galerie = document.querySelector('.galerie')
+    console.log('galerie' + galerie.tagName)
+    let carrousel__figure = document.querySelector('figure')
+    
+    //Création dynamique d'une image du carrousel 
+    let galerie__img = galerie.querySelectorAll('img')
+    console.log("premiere image de la galerie = " + galerie__img.src)
+    let index = 0
+    // carrousel__img.src = galerie__img.src
 
-    bouton.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir')
-    })
+    for (const elm of galerie__img){   
+        creer_image_galerie(index, elm)     
+        creer_radio_carrousel(index)
+        index = index++
+    }
+    
+    /**
+     * Créer l'image du carrousel à partir de la galerie
+     * @param {*} index le numéro de l'image
+     * @param elm l'élément image de la galerie
+     */
+  
+    function creer_image_galerie(index, elm){
+        console.log(elm.src) //vérifier l'adresse url
+        //créer les images du carrousel
+        let carrousel__img = document.createElement('img')
+        carrousel__img.src = elm.src
+        carrousel__img.classList.add('carrousel__img')
+        //ajouter un attribut à la balise html
+        carrousel__img.dataset.index = index
+        carrousel__figure.appendChild(carrousel__img)
+    }
+
         
+    /**
+     * Créer l'image du carrousel à partir de la galerie
+     * @param {*} index le numéro de l'image
+     * @param elm l'élément image de la galerie
+     */
+
+    function creer_radio_carrousel(index){
+        let carrousel_radio = document.createElement('input')
+        //Ajouter une classe 
+        // ajouter un index
+        //ajouter un type radio
+        //spécifier name
+        //ajouter dans carrousel__form
+    }
+
+    /*
+    //Collection d'images de la galerie    
+    console.log("premiere image du carrousel = " + carrousel__img.src)
+    carrousel__figure.appendChild(carrousel__img)
+    console.log(carrousel__figure)
+    */
+
+/* écouteur pour ouvrir la boîte modale */
+    bouton.addEventListener('mousedown', function(){
+        carrousel.classList.add('carrousel--ouvrir') // ouvrir le carrousel
+        console.log("J'ai bien cliqué sur le bouton!!!!!");
+    })
+/* Écouteur pour fermer la boîte modale */
     carrousel__x.addEventListener('mousedown', function(){
-        carrousel.classList.remove('carrousel--ouvrir')
+        carrousel.classList.remove('carrousel--ouvrir') // fermer le carrousel
     })
 
 })()
